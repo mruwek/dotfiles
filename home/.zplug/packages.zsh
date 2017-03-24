788e50243b6226868d2bee7261d17359dd468e81
Jacek Sowiński <mruwek@gmail.com>
2017-03-24 16:38:25 +0100

    zsh: update packages.zsh
    Add colored-man-pages, use upstream version of history-substring-search.
    Also fix order of loading zsh-syntax-highlighting by using "defer" tag
    fo zplug.

--- home/.zplug/packages.zsh
+++ home/.zplug/packages.zsh
@@ -1,5 +1,6 @@
 zplug "plugins/history", from:oh-my-zsh
-zplug "plugins/history-substring-search", from:oh-my-zsh
 zplug "plugins/vi-mode", from:oh-my-zsh
 zplug "zsh-users/zsh-completions"
-zplug "zsh-users/zsh-syntax-highlighting"
+zplug "plugins/colored-man-pages", from:oh-my-zsh
+zplug "zsh-users/zsh-syntax-highlighting", defer:2
+zplug "zsh-users/zsh-history-substring-search", defer:3
