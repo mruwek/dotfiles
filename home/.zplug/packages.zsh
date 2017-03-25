c658db752a1ad7fcb3f04978694a4864ab99ecf7
Jacek Sowiński <mruwek@gmail.com>
2017-03-25 22:54:10 +0100

    zsh: comp generator + comp for homesick

--- home/.zplug/packages.zsh
+++ home/.zplug/packages.zsh
@@ -4,3 +4,5 @@ zplug "zsh-users/zsh-completions"
 zplug "plugins/colored-man-pages", from:oh-my-zsh
 zplug "zsh-users/zsh-syntax-highlighting", defer:2
 zplug "zsh-users/zsh-history-substring-search", defer:3
+zplug "RobSis/zsh-completion-generator"
+zplug "iamthememory/homesick-zsh-completion"
