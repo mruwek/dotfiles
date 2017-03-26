b6c87a766a2bacd056d51c0a7d3d5a43f90f0993
Jacek Sowiński <mruwek@gmail.com>
2017-03-26 03:17:21 +0200

    zsh: add fzf and git-cal

--- home/.zplug/packages.zsh
+++ home/.zplug/packages.zsh
@@ -6,3 +6,6 @@ zplug "zsh-users/zsh-syntax-highlighting", defer:2
 zplug "zsh-users/zsh-history-substring-search", defer:3
 zplug "RobSis/zsh-completion-generator"
 zplug "iamthememory/homesick-zsh-completion"
+zplug "junegunn/fzf", as:command, hook-build:"./install --bin", use:"bin/{fzf-tmux,fzf}"
+zplug "k4rthik/git-cal", as:command
+zplug "junegunn/fzf", use:"shell/*.zsh", defer:2
